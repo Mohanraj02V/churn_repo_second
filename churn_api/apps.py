fb922ad3e144eae14795f044d8e5a375563b6827
MOHANRAJ VELMANI <164868046+Mohanraj02V@users.noreply.github.com>
2025-06-27 13:05:03 +0530

    Delete churn_api directory

--- churn_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChurnApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'churn_api'
--- churn_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PredictChurnAPIView
-
-urlpatterns = [
-    path('predict/', PredictChurnAPIView.as_view(), name='predict-churn'),
-]
