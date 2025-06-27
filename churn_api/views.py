6ac3340d3818bae98da16b0593e64cc9c5366f01
MOHANRAJ VELMANI <164868046+Mohanraj02V@users.noreply.github.com>
2025-06-27 15:42:31 +0530

    Update views.py

--- churn_api/views.py
+++ churn_api/views.py
@@ -142,6 +142,12 @@ class PredictChurnAPIView(APIView):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 '''
+import joblib
+import pandas as pd
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import os
 class PredictChurnAPIView(APIView):
     def post(self, request):
         try:
